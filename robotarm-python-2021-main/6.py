d91603994875234104d882087858e6d1de178ddf
BuildTools <unconfigured@null.spigotmc.org>
2021-09-30 09:45:31 +0200

    Completed level 6.py

---
+++ robotarm-python-2021-main/6.py
@@ -0,0 +1,16 @@
+from RobotArm import RobotArm
+
+
+robotArm = RobotArm('exercise 6')
+
+for i in range(7):
+    robotArm.moveRight()
+
+for i in range(8):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
+
+robotArm.wait(10)
